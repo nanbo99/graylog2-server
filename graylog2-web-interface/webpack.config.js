ca661b27151cc1f9858c9d9f86b9c37ef7902b68
Kyle Knight <kyle@graylog.com>
2019-05-28 09:34:41 -0500

    revert webpack config to master

--- graylog2-web-interface/webpack.config.js
+++ graylog2-web-interface/webpack.config.js
@@ -68,16 +68,14 @@ const webpackConfig = {
         ],
       },
       { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'], exclude: /bootstrap\.less$/ },
-      {
- test: /\.css$/,
+      { test: /\.css$/,
         use: [
           'style-loader',
           {
             loader: 'css-loader',
             options: getCssLoaderOptions(),
           },
-        ]
-},
+        ] },
     ],
   },
   resolve: {
